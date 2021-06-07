eee84e92c947c0f733007151f8f1606e70601d8c
Atul Kesharwani <kesharwaniatul9935@gmail.com>
2021-06-07 09:43:45 +0530

    database connection

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb://localhost:27017/users';
+const CONNECTION_URL = 'mongodb+srv://atul5615853:atul5615853@thapatech.dfmzx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
